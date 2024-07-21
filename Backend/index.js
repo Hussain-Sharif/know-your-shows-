989172bee8cd0552af4ce5a15dfbacc2f75d054e
hussain-sharif <99sharif00@gmail.com>
2024-07-21 21:42:40 +0530

    New APIs Written for Like, Remainder Pages and Their Button Feature APIs as well

--- Backend/index.js
+++ Backend/index.js
@@ -104,6 +104,16 @@
       `
       await db.run(likesViewQuery)
 
+      // Remainders View Query 
+      const remaindersViewQuery=`
+        CREATE TEMP VIEW IF NOT EXISTS user${userid}_remainders_showids AS
+        SELECT show_id,isRemainded
+        FROM 
+        Remainders
+        WHERE user_id=${userid}
+      `
+      await db.run(remaindersViewQuery)
+
       // DB Result For Language
       let dbReuslt;
       const getLanguageQuery = `
@@ -116,10 +126,11 @@
           Shows.start_of_show,
           Shows.end_of_show,
           Shows.language,
-          user${userid}_likes_showids.isLiked
+          user${userid}_likes_showids.isLiked,
+          user${userid}_remainders_showids.isRemainded
           FROM 
           (Shows LEFT JOIN user${userid}_likes_showids ON Shows.id=user${userid}_likes_showids.show_id) AS ShowsWithLikes
-          LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id 
+          LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id LEFT JOIN user${userid}_remainders_showids ON user${userid}_remainders_showids.show_id = Shows.id
           WHERE 
           Shows.language LIKE "%${search}%"
           LIMIT ${limit} OFFSET ${offset};
@@ -139,10 +150,11 @@
             Shows.start_of_show,
             Shows.end_of_show,
             Shows.language,
-          user${userid}_likes_showids.isLiked
+          user${userid}_likes_showids.isLiked,
+          user${userid}_remainders_showids.isRemainded
           FROM 
           (Shows LEFT JOIN user${userid}_likes_showids ON Shows.id=user${userid}_likes_showids.show_id) AS ShowsWithLikes
-          LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id
+          LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id LEFT JOIN user${userid}_remainders_showids ON user${userid}_remainders_showids.show_id = Shows.id
             WHERE 
             Channels.genre Like "%${search}%"
             LIMIT ${limit} OFFSET ${offset};
@@ -163,10 +175,11 @@
             Shows.start_of_show,
             Shows.end_of_show,
             Shows.language,
-          user${userid}_likes_showids.isLiked
+          user${userid}_likes_showids.isLiked,
+          user${userid}_remainders_showids.isRemainded
           FROM 
           (Shows LEFT JOIN user${userid}_likes_showids ON Shows.id=user${userid}_likes_showids.show_id) AS ShowsWithLikes
-          LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id
+          LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id LEFT JOIN user${userid}_remainders_showids ON user${userid}_remainders_showids.show_id = Shows.id
             WHERE 
             (Channels.channel like "%${search}%" OR Shows.show like "%${search}%")
             LIMIT ${limit} OFFSET ${offset};
@@ -180,6 +193,208 @@
 
     });
 
+  // API for Remainders page
+
+  app.get("/all_remainders/", securityCheck,async (request, response) => {    
+    const {search,userid}=request.query;
+    console.log({search,userid})
+
+    // Likes View Query 
+    const likesViewQuery=`
+      CREATE TEMP VIEW IF NOT EXISTS user${userid}_likes_showids AS
+      SELECT show_id,isLiked
+      FROM 
+      Likes
+      WHERE user_id=${userid}
+    `
+    await db.run(likesViewQuery)
+
+    // Remainders View Query 
+    const remaindersViewQuery=`
+      CREATE TEMP VIEW IF NOT EXISTS user${userid}_remainders_showids AS
+      SELECT show_id,isRemainded
+      FROM 
+      Remainders
+      WHERE user_id=${userid}
+    `
+    await db.run(remaindersViewQuery)
+
+    // DB Result For Language
+    let dbReuslt;
+    const getLanguageQuery = `
+      SELECT 
+        Shows.id,
+        Shows.channel_id,
+        Channels.channel,
+        Channels.genre,
+        Shows.show,
+        Shows.start_of_show,
+        Shows.end_of_show,
+        Shows.language,
+        user${userid}_likes_showids.isLiked,
+        user${userid}_remainders_showids.isRemainded
+        FROM 
+        (Shows INNER JOIN user${userid}_remainders_showids  ON Shows.id=user${userid}_remainders_showids.show_id) AS ShowsWithLikes
+        LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id LEFT JOIN user${userid}_likes_showids ON user${userid}_likes_showids.show_id = Shows.id
+        WHERE 
+        Shows.language LIKE "%${search}%";
+        `;
+    dbReuslt = await db.all(getLanguageQuery);
+    console.log("Array Length of Language: ",dbReuslt.length)
+    if(dbReuslt.length===0){
+
+      // DB Result For Genre
+      const getGenreQuery = `
+        SELECT 
+          Shows.id,
+          Shows.channel_id,
+          Channels.channel,
+          Channels.genre,
+          Shows.show,
+          Shows.start_of_show,
+          Shows.end_of_show,
+          Shows.language,
+        user${userid}_likes_showids.isLiked,
+        user${userid}_remainders_showids.isRemainded
+        FROM 
+        (Shows INNER JOIN user${userid}_remainders_showids  ON Shows.id=user${userid}_remainders_showids.show_id) AS ShowsWithLikes
+        LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id LEFT JOIN user${userid}_likes_showids ON user${userid}_likes_showids.show_id = Shows.id
+          WHERE 
+          Channels.genre Like "%${search}%";
+          `;
+      dbReuslt = await db.all(getGenreQuery);
+      console.log("Array Length of Genre's: ",dbReuslt.length)
+    }
+
+    if(dbReuslt.length===0){
+      // DB Result For Show name OR Channel Name
+      const getNamesQuery = `
+        SELECT 
+          Shows.id,
+          Shows.channel_id,
+          Channels.channel,
+          Channels.genre,
+          Shows.show,
+          Shows.start_of_show,
+          Shows.end_of_show,
+          Shows.language,
+        user${userid}_likes_showids.isLiked,
+        user${userid}_remainders_showids.isRemainded
+        FROM 
+        (Shows INNER JOIN user${userid}_remainders_showids  ON Shows.id=user${userid}_remainders_showids.show_id) AS ShowsWithLikes
+        LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id LEFT JOIN user${userid}_likes_showids ON user${userid}_likes_showids.show_id = Shows.id
+          WHERE 
+          (Channels.channel like "%${search}%" OR Shows.show like "%${search}%");
+          `;
+      dbReuslt = await db.all(getNamesQuery);
+      console.log("Array Length of channel,shows Names: ",dbReuslt.length)
+    }
+    
+    response.send(dbReuslt);
+  
+
+  });
+  // API for LIKES page
+
+  app.get("/all_likes/", securityCheck,async (request, response) => {    
+    const {search,userid}=request.query;
+    console.log({search,userid})
+
+    // Likes View Query 
+    const likesViewQuery=`
+      CREATE TEMP VIEW IF NOT EXISTS user${userid}_likes_showids AS
+      SELECT show_id,isLiked
+      FROM 
+      Likes
+      WHERE user_id=${userid}
+    `
+    await db.run(likesViewQuery)
+
+    // Remainders View Query 
+    const remaindersViewQuery=`
+      CREATE TEMP VIEW IF NOT EXISTS user${userid}_remainders_showids AS
+      SELECT show_id,isRemainded
+      FROM 
+      Remainders
+      WHERE user_id=${userid}
+    `
+    await db.run(remaindersViewQuery)
+
+    // DB Result For Language
+    let dbReuslt;
+    const getLanguageQuery = `
+      SELECT 
+        Shows.id,
+        Shows.channel_id,
+        Channels.channel,
+        Channels.genre,
+        Shows.show,
+        Shows.start_of_show,
+        Shows.end_of_show,
+        Shows.language,
+        user${userid}_likes_showids.isLiked,
+        user${userid}_remainders_showids.isRemainded
+        FROM 
+        (Shows LEFT JOIN user${userid}_remainders_showids  ON Shows.id=user${userid}_remainders_showids.show_id) AS ShowsWithLikes
+        LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id INNER JOIN user${userid}_likes_showids ON user${userid}_likes_showids.show_id = Shows.id
+        WHERE 
+        Shows.language LIKE "%${search}%";
+        `;
+    dbReuslt = await db.all(getLanguageQuery);
+    console.log("Array Length of Language: ",dbReuslt.length)
+    if(dbReuslt.length===0){
+
+      // DB Result For Genre
+      const getGenreQuery = `
+        SELECT 
+          Shows.id,
+          Shows.channel_id,
+          Channels.channel,
+          Channels.genre,
+          Shows.show,
+          Shows.start_of_show,
+          Shows.end_of_show,
+          Shows.language,
+        user${userid}_likes_showids.isLiked,
+        user${userid}_remainders_showids.isRemainded
+        FROM 
+        (Shows LEFT JOIN user${userid}_remainders_showids  ON Shows.id=user${userid}_remainders_showids.show_id) AS ShowsWithLikes
+        LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id INNER JOIN user${userid}_likes_showids ON user${userid}_likes_showids.show_id = Shows.id
+          WHERE 
+          Channels.genre Like "%${search}%";
+          `;
+      dbReuslt = await db.all(getGenreQuery);
+      console.log("Array Length of Genre's: ",dbReuslt.length)
+    }
+
+    if(dbReuslt.length===0){
+      // DB Result For Show name OR Channel Name
+      const getNamesQuery = `
+        SELECT 
+          Shows.id,
+          Shows.channel_id,
+          Channels.channel,
+          Channels.genre,
+          Shows.show,
+          Shows.start_of_show,
+          Shows.end_of_show,
+          Shows.language,
+        user${userid}_likes_showids.isLiked,
+        user${userid}_remainders_showids.isRemainded
+        FROM 
+        (Shows LEFT JOIN user${userid}_remainders_showids  ON Shows.id=user${userid}_remainders_showids.show_id) AS ShowsWithLikes
+        LEFT JOIN Channels ON  ShowsWithLikes.channel_id=Channels.id INNER JOIN user${userid}_likes_showids ON user${userid}_likes_showids.show_id = Shows.id
+          WHERE 
+          (Channels.channel like "%${search}%" OR Shows.show like "%${search}%");
+          `;
+      dbReuslt = await db.all(getNamesQuery);
+      console.log("Array Length of channel,shows Names: ",dbReuslt.length)
+    }
+    
+    response.send(dbReuslt);
+  
+
+  });
     
 // API For Sign Up
 
@@ -295,4 +510,72 @@ app.get("/topenglish/",securityCheck,async(request,response)=>{
   `
   const dbResult=await db.all(topEnglishQuery)
   response.send(dbResult)
+})
+
+
+/// <-------POST for Remainders By User
+
+
+app.post("/show_remainded/",securityCheck,async(request,response)=>{
+  const {showid,userid}=request.body
+  console.log("User Remainder",{showid,userid})
+  const likeQuery=`
+    INSERT 
+    INTO
+    Remainders(show_id,user_id,isRemainded)
+    VALUES("${showid}","${userid}","${1}");
+  `
+    await db.run(likeQuery);
+    response.status(200)
+    response.send(JSON.stringify({success_msg:"Added to Remainders"}));
+
+})
+
+// Delete Remainder by the User
+app.delete("/show_remainder_delete/",securityCheck,async(request,response)=>{
+  const {showid,userid}=request.query
+  console.log("User Remainder Reomved",{showid,userid})
+  const likeQuery=`
+    DELETE 
+    FROM 
+    Remainders
+    WHERE user_id=${userid} AND show_id=${showid};
+  `
+   await db.run(likeQuery);
+    response.send(JSON.stringify({success_msg:"Removed From Remainders"}));
+
+})
+
+
+/// <-------POST for Likes By User
+
+
+app.post("/showliked/",securityCheck,async(request,response)=>{
+  const {showid,userid}=request.body
+  console.log("User Liked",{showid,userid})
+  const likeQuery=`
+    INSERT 
+    INTO
+    Likes(show_id,user_id,isLiked)
+    VALUES("${showid}","${userid}","${1}");
+  `
+    await db.run(likeQuery);
+    response.status(200)
+    response.send(JSON.stringify({success_msg:"Like is Added"}));
+
+})
+
+// Delete Like by the User
+app.delete("/showlikedelete/",securityCheck,async(request,response)=>{
+  const {showid,userid}=request.query
+  console.log("User Like Reomved",{showid,userid})
+  const likeQuery=`
+    DELETE 
+    FROM 
+    Likes
+    WHERE user_id=${userid} AND show_id=${showid};
+  `
+   await db.run(likeQuery);
+    response.send(JSON.stringify({success_msg:"Like is Removed"}));
+
 })
